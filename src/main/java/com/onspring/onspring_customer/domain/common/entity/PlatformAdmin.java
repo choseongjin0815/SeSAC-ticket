46e681173d807dd0cfca42cb18d6d323494341fe
lidoca <666ltd@gmail.com>
2025-03-11 17:25:51 +0900

    choir: add PlatformAdmin entity

---
+++ src/main/java/com/onspring/onspring_customer/domain/common/entity/PlatformAdmin.java
@@ -0,0 +1,22 @@
+package com.onspring.onspring_customer.domain.common.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "platform_admin")
+public class PlatformAdmin extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @NotNull
+    @Column(unique = true)
+    private String userName;
+
+}
