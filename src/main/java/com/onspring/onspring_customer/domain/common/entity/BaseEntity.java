7b137e7f2ec1dff4aab20f82116c3ec879fe30e5
lidoca <666ltd@gmail.com>
2025-03-11 17:24:56 +0900

    choir: move the file path naming to `comply` for maintaining consistency

--- src/main/java/com/onspring/onspring_customer/domain/common/entity/BaseEntity.java
+++ src/main/java/com/onspring/onspring_customer/domain/common/entity/BaseEntity.java
@@ -1,4 +1,4 @@
-package com.onspring.onspring_customer.domain.common;
+package com.onspring.onspring_customer.domain.common.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
