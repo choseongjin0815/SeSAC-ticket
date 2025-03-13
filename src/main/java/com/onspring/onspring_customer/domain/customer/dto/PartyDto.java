efe49f7a330229fae6097aee59431502e7544e28
lidoca <666ltd@gmail.com>
2025-03-13 20:04:31 +0900

    chore: add PartyDto

---
+++ src/main/java/com/onspring/onspring_customer/domain/customer/dto/PartyDto.java
@@ -0,0 +1,29 @@
+package com.onspring.onspring_customer.domain.customer.dto;
+
+import lombok.Value;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Value
+public class PartyDto implements Serializable {
+    Long id;
+    String name;
+    LocalDateTime period;
+    BigDecimal amount;
+    LocalTime allowedTimeStart;
+    LocalTime allowedTimeEnd;
+    Long validThru;
+    boolean sunday;
+    boolean monday;
+    boolean tuesday;
+    boolean wednesday;
+    boolean thursday;
+    boolean friday;
+    boolean saturday;
+    BigDecimal maximumAmount;
+    Long maximumTransaction;
+    boolean isActivated;
+}
