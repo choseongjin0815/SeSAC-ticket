0380e677c630905b19e4a85debf222a486f26825
lidoca <666ltd@gmail.com>
2025-03-13 14:25:03 +0900

    chore: add PartyEndUserRepository

---
+++ src/main/java/com/onspring/onspring_customer/domain/common/repository/PartyEndUserRepository.java
@@ -0,0 +1,7 @@
+package com.onspring.onspring_customer.domain.common.repository;
+
+import com.onspring.onspring_customer.domain.common.entity.PartyEndUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PartyEndUserRepository extends JpaRepository<PartyEndUser, Long> {
+}
