17ffed2e4100ec65b1a8d315d9992e0926d1cf0c
lidoca <666ltd@gmail.com>
2025-03-13 14:24:06 +0900

    chore: add AdminRepository

---
+++ src/main/java/com/onspring/onspring_customer/domain/customer/repository/AdminRepository.java
@@ -0,0 +1,7 @@
+package com.onspring.onspring_customer.domain.customer.repository;
+
+import com.onspring.onspring_customer.domain.customer.entity.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdminRepository extends JpaRepository<Admin, Long> {
+}
