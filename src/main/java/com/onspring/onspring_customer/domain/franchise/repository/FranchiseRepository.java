ef601fa19f576492902560df8c4b7e5be4250cef
lidoca <666ltd@gmail.com>
2025-03-13 14:24:55 +0900

    chore: add FranchiseRepository

---
+++ src/main/java/com/onspring/onspring_customer/domain/franchise/repository/FranchiseRepository.java
@@ -0,0 +1,7 @@
+package com.onspring.onspring_customer.domain.franchise.repository;
+
+import com.onspring.onspring_customer.domain.franchise.entity.Franchise;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FranchiseRepository extends JpaRepository<Franchise, Long> {
+}
