2e71c84981be6c6eefad1298b78773c132e87651
lidoca <666ltd@gmail.com>
2025-03-13 14:25:21 +0900

    chore: add PlatformAdminRepository

---
+++ src/main/java/com/onspring/onspring_customer/domain/common/repository/PlatformAdminRepository.java
@@ -0,0 +1,7 @@
+package com.onspring.onspring_customer.domain.common.repository;
+
+import com.onspring.onspring_customer.domain.common.entity.PlatformAdmin;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlatformAdminRepository extends JpaRepository<PlatformAdmin, Long> {
+}
