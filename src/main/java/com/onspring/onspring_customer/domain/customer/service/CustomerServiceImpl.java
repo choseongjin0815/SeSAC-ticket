f733dc72ef8b70db39e2d2b9ef80b4e5340eb883
lidoca <666ltd@gmail.com>
2025-03-16 21:14:23 +0900

    chore(service): add CustomerServiceImpl

---
+++ src/main/java/com/onspring/onspring_customer/domain/customer/service/CustomerServiceImpl.java
@@ -0,0 +1,41 @@
+package com.onspring.onspring_customer.domain.customer.service;
+
+import com.onspring.onspring_customer.domain.customer.dto.CustomerDto;
+import com.onspring.onspring_customer.domain.customer.repository.CustomerRepository;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Setter
+@Getter
+@Service
+public class CustomerServiceImpl implements CustomerService {
+    private CustomerRepository customerRepository;
+
+    @Override
+    public Long saveCustomer(CustomerDto customerDto) {
+        return 0L;
+    }
+
+    @Override
+    public CustomerDto findCustomerById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<CustomerDto> findAllCustomer() {
+        return List.of();
+    }
+
+    @Override
+    public boolean updateCustomer(CustomerDto customerDto) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteCustomerById(Long id) {
+        return false;
+    }
+}
