51496caabefb10131f044d42417ef75acd3e94a2
lidoca <666ltd@gmail.com>
2025-03-16 21:27:45 +0900

    chore(DTO): add Builder from CustomerDto

--- src/main/java/com/onspring/onspring_customer/domain/customer/dto/CustomerDto.java
+++ src/main/java/com/onspring/onspring_customer/domain/customer/dto/CustomerDto.java
@@ -1,10 +1,12 @@
 package com.onspring.onspring_customer.domain.customer.dto;
 
+import lombok.Builder;
 import lombok.Value;
 
 import java.io.Serializable;
 
 @Value
+@Builder
 public class CustomerDto implements Serializable {
     Long id;
     String name;
