46c0c11fa262bb9e126037f59d7dfe1936c10a88
choseongin0815 <chobocho0815@gmail.com>
2025-03-16 16:35:25 +0900

    chore: add JPQL queries to find closed transactions by franchise ID and date range or period

--- src/main/java/com/onspring/onspring_customer/domain/common/repository/TransactionRepository.java
+++ src/main/java/com/onspring/onspring_customer/domain/common/repository/TransactionRepository.java
@@ -11,14 +11,24 @@ import java.util.List;
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
     // 특정 기간 동안의 트랜잭션을 조회하는 JPQL 쿼리
-    @Query("SELECT t FROM Transaction t WHERE t.franchise.id = :franchiseId AND t.transactionTime BETWEEN :startDate AND :endDate")
+    @Query("SELECT t FROM Transaction t WHERE t.franchise.id = :franchiseId AND t.transactionTime BETWEEN :startDate AND :endDate ORDER BY t.transactionTime DESC")
     List<Transaction> findTransactionsByFranchiseIdAndDateRange(@Param("franchiseId") Long franchiseId,
                                                                 @Param("startDate") LocalDateTime startDate,
                                                                 @Param("endDate") LocalDateTime endDate);
 
     // 특정 period에 맞는 트랜잭션을 조회하는 JPQL 쿼리
-    @Query("SELECT t FROM Transaction t WHERE t.franchise.id = :franchiseId AND t.transactionTime BETWEEN :startDate AND :endDate")
+    @Query("SELECT t FROM Transaction t WHERE t.franchise.id = :franchiseId AND t.transactionTime BETWEEN :startDate AND :endDate ORDER BY t.transactionTime DESC")
     List<Transaction> findTransactionsByFranchiseIdAndPeriod(@Param("franchiseId") Long franchiseId,
                                                              @Param("startDate") LocalDateTime startDate,
                                                              @Param("endDate") LocalDateTime endDate);
+
+
+    @Query("SELECT t FROM Transaction t WHERE t.franchise.id = :franchiseId " +
+            "AND t.transactionTime BETWEEN :startDate AND :endDate " +
+            "AND t.isClosed = true " +
+            "ORDER BY t.transactionTime DESC")
+    List<Transaction> findTransactionsByFranchiseIdAndDateRangeAndClosed(
+            @Param("franchiseId") Long franchiseId,
+            @Param("startDate") LocalDateTime startDate,
+            @Param("endDate") LocalDateTime endDate);
 }
