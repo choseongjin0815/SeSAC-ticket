3e2d1e333cb1c7ad5b16689a89c59124ac3783ab
lidoca <666ltd@gmail.com>
2025-03-16 21:56:55 +0900

    chore(service): add Builder to AdminDto

--- src/main/java/com/onspring/onspring_customer/domain/customer/dto/AdminDto.java
+++ src/main/java/com/onspring/onspring_customer/domain/customer/dto/AdminDto.java
@@ -1,12 +1,15 @@
 package com.onspring.onspring_customer.domain.customer.dto;
 
+import lombok.Builder;
 import lombok.Value;
 
 import java.io.Serializable;
 
 @Value
+@Builder
 public class AdminDto implements Serializable {
     Long id;
+    Long customerId;
     String userName;
     boolean isSuperAdmin;
 }
