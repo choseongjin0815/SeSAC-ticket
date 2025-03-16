9a2486e100f322f2f87391e561e80a54b6e5c048
lidoca <666ltd@gmail.com>
2025-03-16 21:57:27 +0900

    feat(service): implement createAdmin

--- src/main/java/com/onspring/onspring_customer/domain/customer/service/AdminServiceImpl.java
+++ src/main/java/com/onspring/onspring_customer/domain/customer/service/AdminServiceImpl.java
@@ -1,7 +1,10 @@
 package com.onspring.onspring_customer.domain.customer.service;
 
 import com.onspring.onspring_customer.domain.customer.dto.AdminDto;
+import com.onspring.onspring_customer.domain.customer.entity.Admin;
+import com.onspring.onspring_customer.domain.customer.entity.Customer;
 import com.onspring.onspring_customer.domain.customer.repository.AdminRepository;
+import com.onspring.onspring_customer.domain.customer.repository.CustomerRepository;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.stereotype.Service;
@@ -13,10 +16,20 @@ import java.util.List;
 @Service
 public class AdminServiceImpl implements AdminService {
     private AdminRepository adminRepository;
+    private CustomerRepository customerRepository;
 
     @Override
     public Long createAdmin(AdminDto adminDto) {
-        return 0L;
+        Customer customer = customerRepository.findById(adminDto.getCustomerId())
+                .orElseThrow();
+        Admin admin = Admin.builder()
+                .customer(customer)
+                .userName(adminDto.getUserName())
+                .isSuperAdmin(adminDto.isSuperAdmin())
+                .build();
+
+        return adminRepository.save(admin)
+                .getId();
     }
 
     @Override
