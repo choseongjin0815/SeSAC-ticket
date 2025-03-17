6be1fd78414953b2f312c22d59543529d441a355
lidoca <666ltd@gmail.com>
2025-03-17 10:44:38 +0900

    feat(service): implement saveParty

--- src/main/java/com/onspring/onspring_customer/domain/customer/service/PartyServiceImpl.java
+++ src/main/java/com/onspring/onspring_customer/domain/customer/service/PartyServiceImpl.java
@@ -1,6 +1,9 @@
 package com.onspring.onspring_customer.domain.customer.service;
 
 import com.onspring.onspring_customer.domain.customer.dto.PartyDto;
+import com.onspring.onspring_customer.domain.customer.entity.Customer;
+import com.onspring.onspring_customer.domain.customer.entity.Party;
+import com.onspring.onspring_customer.domain.customer.repository.CustomerRepository;
 import com.onspring.onspring_customer.domain.customer.repository.PartyRepository;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,10 +16,34 @@ import java.util.List;
 @Service
 public class PartyServiceImpl implements PartyService {
     private PartyRepository partyRepository;
+    private CustomerRepository customerRepository;
 
     @Override
     public Long saveParty(PartyDto partyDto) {
-        return 0L;
+        Customer customer = customerRepository.findById(partyDto.getCustomerId())
+                .orElseThrow();
+        Party party = Party.builder()
+                .customer(customer)
+                .name(partyDto.getName())
+                .period(partyDto.getPeriod())
+                .amount(partyDto.getAmount())
+                .allowedTimeStart(partyDto.getAllowedTimeStart())
+                .allowedTimeEnd(partyDto.getAllowedTimeEnd())
+                .validThru(partyDto.getValidThru())
+                .sunday(partyDto.isSunday())
+                .monday(partyDto.isMonday())
+                .tuesday(partyDto.isTuesday())
+                .wednesday(partyDto.isWednesday())
+                .thursday(partyDto.isThursday())
+                .friday(partyDto.isFriday())
+                .saturday(partyDto.isSaturday())
+                .maximumAmount(partyDto.getMaximumAmount())
+                .maximumTransaction(partyDto.getMaximumTransaction())
+                .isActivated(partyDto.isActivated())
+                .build();
+
+        return partyRepository.save(party)
+                .getId();
     }
 
     @Override
