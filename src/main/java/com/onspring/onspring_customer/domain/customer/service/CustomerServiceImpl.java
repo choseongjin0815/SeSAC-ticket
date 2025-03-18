3a7c55839d647bce9adaff09f45ca5839c21c8cc
lidoca <666ltd@gmail.com>
2025-03-18 22:02:55 +0900

    fix(service): add Log4j to CustomerServiceImpl

--- src/main/java/com/onspring/onspring_customer/domain/customer/service/CustomerServiceImpl.java
+++ src/main/java/com/onspring/onspring_customer/domain/customer/service/CustomerServiceImpl.java
@@ -3,11 +3,13 @@ package com.onspring.onspring_customer.domain.customer.service;
 import com.onspring.onspring_customer.domain.customer.dto.CustomerDto;
 import com.onspring.onspring_customer.domain.customer.entity.Customer;
 import com.onspring.onspring_customer.domain.customer.repository.CustomerRepository;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Log4j2
 @Service
 public class CustomerServiceImpl implements CustomerService {
     private CustomerRepository customerRepository;
