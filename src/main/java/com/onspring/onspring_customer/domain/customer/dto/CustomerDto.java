8d400b90ae594cb808abc8b67b440ed18f9699d2
lidoca <666ltd@gmail.com>
2025-03-18 22:00:07 +0900

    chore(entity): add constraints on CustomerDto

--- src/main/java/com/onspring/onspring_customer/domain/customer/dto/CustomerDto.java
+++ src/main/java/com/onspring/onspring_customer/domain/customer/dto/CustomerDto.java
@@ -1,5 +1,7 @@
 package com.onspring.onspring_customer.domain.customer.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Value;
 
@@ -9,8 +11,18 @@ import java.io.Serializable;
 @Builder
 public class CustomerDto implements Serializable {
     Long id;
+
+    @NotNull
+    @Size(min = 1)
     String name;
+
+    @NotNull
+    @Size(min = 1)
     String address;
+
+    @NotNull
+    @Size(min = 1)
     String phone;
+
     boolean isActivated;
 }
