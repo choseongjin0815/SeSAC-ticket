c801555961e02d926729134dfedefb716c3088fe
lidoca <666ltd@gmail.com>
2025-03-19 13:49:33 +0900

    style(DTO): set 2 line breaks between each fields from EdnUserDto

--- src/main/java/com/onspring/onspring_customer/domain/user/dto/EndUserDto.java
+++ src/main/java/com/onspring/onspring_customer/domain/user/dto/EndUserDto.java
@@ -8,10 +8,16 @@ import java.math.BigDecimal;
 @Value
 public class EndUserDto implements Serializable {
     Long id;
+
     Long partyId;
+
     String password;
+
     String name;
+
     String phone;
+
     boolean isActivated;
+
     BigDecimal currentPoint;
 }
