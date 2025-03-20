61d3b9fba5b69bd5cfc8aa422f2be3887907a9f1
choseongin0815 <chobocho0815@gmail.com>
2025-03-20 19:47:35 +0900

    fix(DTO): change FranchiseDto, EndUserDto to Long type

--- src/main/java/com/onspring/onspring_customer/domain/common/dto/TransactionDto.java
+++ src/main/java/com/onspring/onspring_customer/domain/common/dto/TransactionDto.java
@@ -1,7 +1,5 @@
 package com.onspring.onspring_customer.domain.common.dto;
 
-import com.onspring.onspring_customer.domain.franchise.dto.FranchiseDto;
-import com.onspring.onspring_customer.domain.user.dto.EndUserDto;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -11,8 +9,10 @@ import java.time.LocalDateTime;
 @Data
 public class TransactionDto implements Serializable {
     Long id;
-    FranchiseDto franchiseDto;
-    EndUserDto endUserDto;
+//    FranchiseDto franchiseDto;
+//    EndUserDto endUserDto;
+    Long userId;
+    Long franchiseId;
     LocalDateTime transactionTime;
     BigDecimal amount;
     boolean isAccepted;
