793934864388bffffc77a01b9d9e6ecb695b4bb5
choseongin0815 <chobocho0815@gmail.com>
2025-03-20 18:07:05 +0900

    chore(repository): add findAllFranchiseByEndUserId to FranchiseRepository

--- src/main/java/com/onspring/onspring_customer/domain/franchise/repository/FranchiseRepository.java
+++ src/main/java/com/onspring/onspring_customer/domain/franchise/repository/FranchiseRepository.java
@@ -1,8 +1,16 @@
 package com.onspring.onspring_customer.domain.franchise.repository;
 
 import com.onspring.onspring_customer.domain.franchise.entity.Franchise;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaRepository;import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface FranchiseRepository extends JpaRepository<Franchise, Long> {
 
+    @Query("SELECT f FROM Franchise f " +
+            "JOIN CustomerFranchise cf ON cf.franchise.id = f.id " +
+            "JOIN Party p ON p.customer.id = cf.customer.id " +
+            "JOIN PartyEndUser peu ON peu.party.customer.id = p.customer.id " +
+            "WHERE peu.endUser.id = :endUserId")
+    List<Franchise> findAllFranchiseByEndUserId(Long endUserId);
 }
