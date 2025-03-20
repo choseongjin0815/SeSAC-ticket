dd69ec4175c896377c6cfca90e310b0ceab660d4
choseongin0815 <chobocho0815@gmail.com>
2025-03-20 12:31:01 +0900

    chore(repository): add findByEndUserId to PointRepository

--- src/main/java/com/onspring/onspring_customer/domain/user/repository/PointRepository.java
+++ src/main/java/com/onspring/onspring_customer/domain/user/repository/PointRepository.java
@@ -3,5 +3,10 @@ package com.onspring.onspring_customer.domain.user.repository;
 import com.onspring.onspring_customer.domain.user.entity.Point;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PointRepository extends JpaRepository<Point, Long> {
+
+
+    List<Point> findByEndUserId(Long endUserId);
 }
