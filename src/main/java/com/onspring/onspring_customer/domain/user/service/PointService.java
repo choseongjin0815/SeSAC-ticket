cd64725e85adb242f313b1ce2f7c649b5fbbd2fd
choseongin0815 <chobocho0815@gmail.com>
2025-03-20 12:28:28 +0900

    chore(service): add getPointsByEndUserId to PointService

--- src/main/java/com/onspring/onspring_customer/domain/user/service/PointService.java
+++ src/main/java/com/onspring/onspring_customer/domain/user/service/PointService.java
@@ -1,4 +1,9 @@
 package com.onspring.onspring_customer.domain.user.service;
 
+import com.onspring.onspring_customer.domain.user.dto.PointResponseDto;
+
+import java.util.List;
+
 public interface PointService {
+    List<PointResponseDto> getPointsByEndUserId(Long endUserId);
 }
