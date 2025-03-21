7a25e3f6597a91c2d4e4a32f9975b5e19d2c4110
yeonju <yeonju9707@naver.com>
2025-03-21 13:34:22 +0900

    feature(ui): Thymeleaf 헤더, 네비게이터(main menu) 생성

---
+++ src/main/java/com/onspring/onspring_customer/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.onspring.onspring_customer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("userName", "홍길동");
+        return "layout";
+    }
+}
