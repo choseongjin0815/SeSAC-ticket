3b6ed04a5f3c74ceed5f2252f037fa43608f7163
lidoca <666ltd@gmail.com>
2025-03-26 14:43:02 +0900

    fix(entity): add builder to Point

--- src/main/java/com/onspring/onspring_customer/domain/user/entity/Point.java
+++ src/main/java/com/onspring/onspring_customer/domain/user/entity/Point.java
@@ -4,16 +4,18 @@ import com.onspring.onspring_customer.domain.common.entity.BaseEntity;
 import com.onspring.onspring_customer.domain.customer.entity.Party;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+@Builder
 @Getter
 @Setter
 @Entity
 @Table(name = "point")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Point extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
