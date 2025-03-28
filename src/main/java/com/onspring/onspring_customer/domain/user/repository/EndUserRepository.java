6c32de0997f24c50a756faee34dadfedc7659e6b
choseongin0815 <chobocho0815@gmail.com>
2025-03-28 13:00:25 +0900

    chore(repository): add findByPhone to EndUserRepository

--- src/main/java/com/onspring/onspring_customer/domain/user/repository/EndUserRepository.java
+++ src/main/java/com/onspring/onspring_customer/domain/user/repository/EndUserRepository.java
@@ -4,4 +4,5 @@ import com.onspring.onspring_customer.domain.user.entity.EndUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EndUserRepository extends JpaRepository<EndUser, Long> {
+    EndUser findByPhone(String phone);
 }
