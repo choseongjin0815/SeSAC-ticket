e655a5107096485c8407d5fb4c390f0fa20ae888
choseongin0815 <chobocho0815@gmail.com>
2025-03-28 13:01:07 +0900

    chore(Dto): add LoginRequestDto

---
+++ src/main/java/com/onspring/onspring_customer/global/auth/dto/LoginRequestDto.java
@@ -0,0 +1,12 @@
+package com.onspring.onspring_customer.global.auth.dto;
+
+public class LoginRequestDto {
+    private String userName;
+    private String phone;
+    private String password;
+
+    // Getters and setters
+    public String getUserName() { return userName; }
+    public String getPhone() { return phone; }
+    public String getPassword() { return password; }
+}
