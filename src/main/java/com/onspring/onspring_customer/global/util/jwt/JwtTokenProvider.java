6f66ce1a5c1fcf1876d254bff25eb7b39aa96a80
choseongin0815 <chobocho0815@gmail.com>
2025-03-30 21:52:14 +0900

    fix(token): Refactor logic in JwtTokenProvider

--- src/main/java/com/onspring/onspring_customer/global/util/jwt/JwtTokenProvider.java
+++ src/main/java/com/onspring/onspring_customer/global/util/jwt/JwtTokenProvider.java
@@ -1,9 +1,11 @@
 package com.onspring.onspring_customer.global.util.jwt;
 
+import com.onspring.onspring_customer.security.CustomUserDetails;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -16,32 +18,117 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Date;
 
+@Log4j2
 @Component
 public class JwtTokenProvider {
 
     private final Key secretKey;
     private final long tokenValidityInMilliseconds;
+    private final long refreshTokenValidityInMilliseconds;
 
     public JwtTokenProvider(
             @Value("${jwt.secret}") String secretKeyString,
-            @Value("${jwt.token-validity-in-seconds}") long tokenValidityInSeconds
+            @Value("${jwt.token-validity-in-seconds}") long tokenValidityInSeconds,
+            @Value("${jwt.refresh-token-validity-in-seconds}") long refreshTokenValidityInSeconds
     ) {
         this.secretKey = Keys.hmacShaKeyFor(secretKeyString.getBytes());
         this.tokenValidityInMilliseconds = tokenValidityInSeconds * 1000;
+        this.refreshTokenValidityInMilliseconds = refreshTokenValidityInSeconds * 1000;
     }
 
-    public String createToken(String username, String role) {
+    public String createToken(Long id, String username, String role) { // userId 파라미터 추가
         Date now = new Date();
         Date validity = new Date(now.getTime() + tokenValidityInMilliseconds);
 
         return Jwts.builder()
                 .setSubject(username)
                 .claim("role", role)
+                .claim("id", id) // userId 클레임 추가
                 .setIssuedAt(now)
                 .setExpiration(validity)
                 .signWith(secretKey, SignatureAlgorithm.HS256)
                 .compact();
     }
+
+    public String createRefreshToken(Long id, String username, String role) { // userId 파라미터 추가
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + refreshTokenValidityInMilliseconds);
+
+        return Jwts.builder()
+                .setSubject(username)
+                .claim("role", role)
+                .claim("id", id) // userId 클레임 추가
+                .setIssuedAt(now)
+                .setExpiration(validity)
+                .signWith(secretKey, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+
+//    public String createToken(String username, String role) {
+//        Date now = new Date();
+//        Date validity = new Date(now.getTime() + tokenValidityInMilliseconds);
+//
+//        return Jwts.builder()
+//                .setSubject(username)
+//                .claim("role", role)
+//                .setIssuedAt(now)
+//                .setExpiration(validity)
+//                .signWith(secretKey, SignatureAlgorithm.HS256)
+//                .compact();
+//    }
+//
+//
+//    public String createRefreshToken(String username, String role) {
+//        Date now = new Date();
+//
+//        Date validity = new Date(now.getTime() + refreshTokenValidityInMilliseconds);
+//
+//        return Jwts.builder()
+//                .setSubject(username)
+//                .claim("role", role)
+//                .setIssuedAt(now)
+//                .setExpiration(validity)
+//                .signWith(secretKey, SignatureAlgorithm.HS256)
+//                .compact();
+//    }
+
+    public String reissueAccessToken(String refreshToken) {
+        if (validateToken(refreshToken)) {
+            Claims claims = Jwts.parserBuilder()
+                    .setSigningKey(secretKey)
+                    .build()
+                    .parseClaimsJws(refreshToken)
+                    .getBody();
+
+            Long id = claims.get("id", Long.class); // userId 추출
+            String username = claims.getSubject();
+            String role = claims.get("role", String.class);
+
+            return createToken(id, username, role); // userId 포함 생성
+        }
+        throw new RuntimeException("Invalid Refresh Token");
+    }
+
+
+//    public String reissueAccessToken(String refreshToken) {
+//        if (validateToken(refreshToken)) {
+//            log.info("Refresh token received: {}", refreshToken);
+//            Claims claims = Jwts.parserBuilder()
+//                    .setSigningKey(secretKey)
+//                    .build()
+//                    .parseClaimsJws(refreshToken)
+//                    .getBody();
+//
+//            String username = claims.getSubject();
+//            String role = claims.get("role", String.class);
+//
+//            log.info("userName: {}", username);
+//            log.info("role: {}", role);
+//            return createToken(username, role);
+//        }
+//        throw new RuntimeException("Invalid Refresh Token");
+//    }
 
     public Authentication getAuthentication(String token) {
         Claims claims = Jwts.parserBuilder()
@@ -50,15 +137,40 @@ public class JwtTokenProvider {
                 .parseClaimsJws(token)
                 .getBody();
 
-        Collection<? extends GrantedAuthority> authorities =
-                Arrays.asList(new SimpleGrantedAuthority(claims.get("role", String.class)));
+        Long id = claims.get("id", Long.class); // 클레임에서 id 추출
+        String username = claims.getSubject(); // 클레임에서 username 추출
+        String role = claims.get("role", String.class); // 클레임에서 role 추출
 
+        Collection<? extends GrantedAuthority> authorities =
+                Arrays.asList(new SimpleGrantedAuthority(role));
+
+        // CustomUserDetails 객체 생성
+        CustomUserDetails customUserDetails = new CustomUserDetails(id, username, role, authorities);
+        log.info(customUserDetails);
         return new UsernamePasswordAuthenticationToken(
-                claims.getSubject(),
-                "",
+                customUserDetails, // principal에 CustomUserDetails 설정
+                null,
                 authorities
         );
     }
+
+
+//    public Authentication getAuthentication(String token) {
+//        Claims claims = Jwts.parserBuilder()
+//                .setSigningKey(secretKey)
+//                .build()
+//                .parseClaimsJws(token)
+//                .getBody();
+//
+//        Collection<? extends GrantedAuthority> authorities =
+//                Arrays.asList(new SimpleGrantedAuthority(claims.get("role", String.class)));
+//
+//        return new UsernamePasswordAuthenticationToken(
+//                claims.getSubject(),
+//                "",
+//                authorities
+//        );
+//    }
 
     public boolean validateToken(String token) {
         try {
