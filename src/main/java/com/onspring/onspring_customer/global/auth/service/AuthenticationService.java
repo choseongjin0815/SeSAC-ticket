95772686c59e958b99c6ad814548c64b8e5ed5dd
choseongin0815 <chobocho0815@gmail.com>
2025-03-30 21:49:07 +0900

    fix(service): Refactor logic in Login

--- src/main/java/com/onspring/onspring_customer/global/auth/service/AuthenticationService.java
+++ src/main/java/com/onspring/onspring_customer/global/auth/service/AuthenticationService.java
@@ -1,11 +1,15 @@
 package com.onspring.onspring_customer.global.auth.service;
 
+import com.onspring.onspring_customer.domain.franchise.dto.FranchiseDto;
 import com.onspring.onspring_customer.domain.franchise.entity.Franchise;
 import com.onspring.onspring_customer.domain.franchise.repository.FranchiseRepository;
 import com.onspring.onspring_customer.domain.user.entity.EndUser;
 import com.onspring.onspring_customer.domain.user.repository.EndUserRepository;
+import com.onspring.onspring_customer.global.auth.dto.LoginResponseDto;
 import com.onspring.onspring_customer.global.util.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -17,25 +21,80 @@ public class AuthenticationService {
     private final EndUserRepository endUserRepository;
     private final JwtTokenProvider jwtTokenProvider;
     private final PasswordEncoder passwordEncoder;
+    private final ModelMapper modelMapper;
 
-    public String franchiseLogin(String userName, String password) {
+    public LoginResponseDto franchiseLogin(String userName, String password) {
+
         Franchise franchise = franchiseRepository.findByUserName(userName);
 
 
         if (!passwordEncoder.matches(password, franchise.getPassword())) {
-            throw new RuntimeException("Invalid password");
+            throw new BadCredentialsException("잘못된 비밀번호");
         }
 
-        return jwtTokenProvider.createToken(userName, "ROLE_FRANCHISE");
+        String accessToken = jwtTokenProvider.createToken(
+                franchise.getId(),
+                franchise.getUserName(),
+                "ROLE_FRANCHISE"
+        );
+        String refreshToken = jwtTokenProvider.createRefreshToken(
+                franchise.getId(),
+                franchise.getUserName(),
+                "ROLE_FRANCHISE"
+        );
+
+        FranchiseDto franchiseDto = modelMapper.map(franchise, FranchiseDto.class);
+
+        return new LoginResponseDto(accessToken, refreshToken, franchise.getId());
     }
 
-    public String userLogin(String phone, String password) {
+    public LoginResponseDto userLogin(String phone, String password) {
+
         EndUser endUser = endUserRepository.findByPhone(phone);
 
         if (!passwordEncoder.matches(password, endUser.getPassword())) {
-            throw new RuntimeException("Invalid password");
+            throw new BadCredentialsException("잘못된 비밀번호");
         }
 
-        return jwtTokenProvider.createToken(phone, "ROLE_USER");
+        String accessToken = jwtTokenProvider.createToken(
+                endUser.getId(),
+                endUser.getPhone(),
+                "ROLE_USER"
+        );
+        String refreshToken = jwtTokenProvider.createRefreshToken(
+                endUser.getId(),
+                endUser.getPhone(),
+                "ROLE_USER"
+        );
+
+        return new LoginResponseDto(accessToken, refreshToken, endUser.getId());
     }
+
+
+//    public LoginResponseDto franchiseLogin(String userName, String password) {
+//        Franchise franchise = franchiseRepository.findByUserName(userName);
+//
+//        if (!passwordEncoder.matches(password, franchise.getPassword())) {
+//            throw new RuntimeException("Invalid password");
+//        }
+//
+//        String accessToken = jwtTokenProvider.createToken(userName, "ROLE_FRANCHISE");
+//        String refreshToken = jwtTokenProvider.createRefreshToken(userName, "ROLE_FRANCHISE");
+//        FranchiseDto franchiseDto = modelMapper.map(franchise, FranchiseDto.class);
+//
+//        return new LoginResponseDto(accessToken, refreshToken, franchise.getId());
+//    }
+//
+//    public LoginResponseDto userLogin(String phone, String password) {
+//        EndUser endUser = endUserRepository.findByPhone(phone);
+//
+//        if (!passwordEncoder.matches(password, endUser.getPassword())) {
+//            throw new RuntimeException("Invalid password");
+//        }
+//
+//        String accessToken = jwtTokenProvider.createToken(phone, "ROLE_USER");
+//        String refreshToken = jwtTokenProvider.createRefreshToken(phone, "ROLE_USER");
+//
+//        return new LoginResponseDto(accessToken, refreshToken, endUser.getId());
+//    }
 }
