312c42da42d898dbf0be6e5be1d54d916779de60
choseongin0815 <chobocho0815@gmail.com>
2025-04-01 19:19:14 +0900

    fix(controller): change return type List to Page

--- src/main/java/com/onspring/onspring_customer/domain/user/controller/UserTransactionController.java
+++ src/main/java/com/onspring/onspring_customer/domain/user/controller/UserTransactionController.java
@@ -5,13 +5,17 @@ import com.onspring.onspring_customer.domain.common.service.TransactionService;
 import com.onspring.onspring_customer.domain.franchise.dto.FranchiseDto;
 import com.onspring.onspring_customer.domain.franchise.service.FranchiseService;
 import com.onspring.onspring_customer.domain.user.service.EndUserService;
+import com.onspring.onspring_customer.security.SecurityUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
-import java.util.List;
 
 @RequiredArgsConstructor
 @Log4j2
@@ -23,14 +27,18 @@ public class UserTransactionController {
     private final FranchiseService franchiseService;
     private final EndUserService endUserService;
 
+
+    //사용자의 결제내역 출력
     @GetMapping("")
-    public ResponseEntity<List<TransactionDto>> getUserTransactions() {
-        Long id = 1L; //스프링 시큐리티 적용 전이므로 테스트용 아이디
-        List<TransactionDto> transactionDtoList = transactionService.findTransactionByEndUserId(id);
-
-        return ResponseEntity.ok(transactionDtoList);
+    public ResponseEntity<Page<TransactionDto>> getUserTransactions(
+            @PageableDefault(sort = "transactionTime", direction = Sort.Direction.DESC) Pageable pageable
+    ) {
+        log.info(pageable);
+        Long userId = SecurityUtil.getCurrentUserId(); // 여기서 userId 추출
+        Page<TransactionDto> result = transactionService.findTransactionByEndUserId(userId, pageable);
+        return ResponseEntity.ok(result);
     }
-
+    //사용자의 가맹점 결제
     @PostMapping("/{franchiseId}")
     public ResponseEntity<Long> addTransaction(@PathVariable Long franchiseId,
                                                @RequestBody TransactionDto transactionDto) {
@@ -39,6 +47,7 @@ public class UserTransactionController {
         log.info(transactionDto);
         log.info(franchiseId);
         FranchiseDto franchiseDto = franchiseService.findFranchiseById(franchiseId);
+
         //MultiPart필드를 null로 초기화
         franchiseDto.setFiles(new ArrayList<>());
 
