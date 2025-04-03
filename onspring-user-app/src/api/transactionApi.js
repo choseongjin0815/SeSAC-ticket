7b49458bad8bcd6b393948ea330cac04bb5d6064
choseongin0815 <chobocho0815@gmail.com>
2025-04-03 20:01:10 +0900

    feat: 결제 API

--- onspring-user-app/src/api/transactionApi.js
+++ onspring-user-app/src/api/transactionApi.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const API_SERVER_HOST = 'http://localhost:8080'; 
 const prefix = `${API_SERVER_HOST}/api/user`;
 
-export const getTransactionList = async (page = 0, size = 15) => {
+export const getTransactionList = async (page = 0, size = 10) => {
   try {
     const res = await axios.get(`${prefix}/transactions`, {
       params: { page, size },
@@ -13,5 +13,19 @@ export const getTransactionList = async (page = 0, size = 15) => {
   } catch (error) {
     console.log('API 호출 실패', error);
     return null;
+  }
+};
+
+export const postTransaction = async (franchiseId, pointId, transactionDto, partyId) => {
+  try {
+    const res = await axios.post(
+      `${prefix}/transactions/${franchiseId}/point/${pointId}`,
+      transactionDto,
+      { params: { partyId } }
+    );
+    return res.data;
+  } catch (error) {
+    console.error('결제 요청 실패:', error);
+    throw error.response?.data || '결제 요청 중 오류 발생';
   }
 };
