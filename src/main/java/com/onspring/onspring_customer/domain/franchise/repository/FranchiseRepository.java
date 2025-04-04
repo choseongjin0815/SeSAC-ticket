ceb2341da39c2bdcab1edc7f15ff3ab89c82b247
choseongin0815 <chobocho0815@gmail.com>
2025-04-04 09:47:05 +0900

    fix(repository): change return type findAllFranchiseByEndUserId List to Page

--- src/main/java/com/onspring/onspring_customer/domain/franchise/repository/FranchiseRepository.java
+++ src/main/java/com/onspring/onspring_customer/domain/franchise/repository/FranchiseRepository.java
@@ -1,19 +1,20 @@
 package com.onspring.onspring_customer.domain.franchise.repository;
 
 import com.onspring.onspring_customer.domain.franchise.entity.Franchise;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
+import org.springframework.data.repository.query.Param;
 
 public interface FranchiseRepository extends JpaRepository<Franchise, Long> {
 
-    @Query("SELECT f FROM Franchise f " +
-            "JOIN CustomerFranchise cf ON cf.franchise.id = f.id " +
-            "JOIN Party p ON p.customer.id = cf.customer.id " +
-            "JOIN PartyEndUser peu ON peu.party.customer.id = p.customer.id " +
+    @Query("SELECT DISTINCT f FROM Franchise f " +
+            "JOIN CustomerFranchise cf ON cf.franchise = f " +
+            "JOIN Party p ON p.customer = cf.customer " +
+            "JOIN PartyEndUser peu ON peu.party = p " +
             "WHERE peu.endUser.id = :endUserId")
-    List<Franchise> findAllFranchiseByEndUserId(Long endUserId);
+    Page<Franchise> findAllFranchiseByEndUserId(@Param("endUserId") Long endUserId, Pageable pageable);
 
     Franchise findByUserName(String userName);
 }
