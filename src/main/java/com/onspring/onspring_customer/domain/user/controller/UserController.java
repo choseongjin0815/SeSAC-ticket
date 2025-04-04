b47538cc855de9d399af52bae5f378f80626f421
choseongin0815 <chobocho0815@gmail.com>
2025-04-04 09:44:47 +0900

    feat(controller): add updateUserPassword

--- src/main/java/com/onspring/onspring_customer/domain/user/controller/UserController.java
+++ src/main/java/com/onspring/onspring_customer/domain/user/controller/UserController.java
@@ -2,13 +2,13 @@ package com.onspring.onspring_customer.domain.user.controller;
 
 import com.onspring.onspring_customer.domain.user.dto.EndUserDto;
 import com.onspring.onspring_customer.domain.user.service.EndUserService;
+import com.onspring.onspring_customer.global.auth.dto.PasswordUpdateRequest;
 import com.onspring.onspring_customer.security.SecurityUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Log4j2
@@ -28,5 +28,23 @@ public class UserController {
         log.info("end user info: {}", endUserDto);
 
         return ResponseEntity.ok(endUserDto);
+    }
+
+
+    @PutMapping("/password")
+    public ResponseEntity<String> updateUserPassword(
+            @RequestBody PasswordUpdateRequest request) {
+
+        log.info("User password update request: {}", request);
+        Long userId = SecurityUtil.getCurrentUserId();
+
+        boolean success = endUserService.updateEndUserPasswordById(userId, request.getOldPassword(), request.getNewPassword());
+
+        if (success) {
+            log.info("User password updated successfully");
+            return ResponseEntity.ok("password updated successfully");
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("password update failed");
+        }
     }
 }
