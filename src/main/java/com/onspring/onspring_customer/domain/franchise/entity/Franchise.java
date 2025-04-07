8f4c9332cb47a46f2dc8c83cc5081f6e2a666e57
choseongin0815 <chobocho0815@gmail.com>
2025-04-07 10:25:53 +0900

    fix(entity): add latitude, longitude and description to Franchise

--- src/main/java/com/onspring/onspring_customer/domain/franchise/entity/Franchise.java
+++ src/main/java/com/onspring/onspring_customer/domain/franchise/entity/Franchise.java
@@ -44,9 +44,19 @@ public class Franchise extends BaseEntity {
     @Column(unique = true)
     private String address;
 
+    @Column
+    private Double latitude;
+
+    @Column
+    private Double longitude;
+
+
     @NotNull
     @Column(unique = true)
     private String phone;
+
+    @Column
+    private String description;
 
     @NotNull
     private boolean isActivated;
@@ -83,6 +93,7 @@ public class Franchise extends BaseEntity {
                 .phone(this.phone)
                 .businessNumber(this.businessNumber)
                 .ownerName(this.ownerName)
+                .description(this.description)
                 .build();
 
         // 이미지 파일 리스트 변환
