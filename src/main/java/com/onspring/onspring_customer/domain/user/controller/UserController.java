7b2b89c5fef1c8085e6ddf58b6ec35c866287826
choseongin0815 <chobocho0815@gmail.com>
2025-04-09 20:11:08 +0900

    feat(controller): add getMenu(image)

--- src/main/java/com/onspring/onspring_customer/domain/user/controller/UserController.java
+++ src/main/java/com/onspring/onspring_customer/domain/user/controller/UserController.java
@@ -1,13 +1,15 @@
 package com.onspring.onspring_customer.domain.user.controller;
 
+import com.onspring.onspring_customer.domain.auth.dto.PasswordUpdateRequest;
 import com.onspring.onspring_customer.domain.customer.dto.PartyDto;
 import com.onspring.onspring_customer.domain.customer.service.PartyService;
 import com.onspring.onspring_customer.domain.user.dto.EndUserDto;
 import com.onspring.onspring_customer.domain.user.service.EndUserService;
-import com.onspring.onspring_customer.domain.auth.dto.PasswordUpdateRequest;
+import com.onspring.onspring_customer.global.util.file.CustomFileUtil;
 import com.onspring.onspring_customer.security.SecurityUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +21,7 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
     private final EndUserService endUserService;
     private final PartyService partyService;
+    private final CustomFileUtil customFileUtil;
 
     //사용자의 정보
     @GetMapping("/info")
@@ -42,6 +45,15 @@ public class UserController {
         PartyDto partyDto = partyService.findPartyByUserId(userId);
 
         return ResponseEntity.ok(partyDto);
+    }
+
+    //메뉴 사진 조회
+    @GetMapping("/menu/{fileName}")
+    public ResponseEntity<Resource> getMenu(@PathVariable String fileName) {
+
+        log.info("get menu: {}", fileName);
+
+        return customFileUtil.getFile(fileName);
     }
 
     //사용자의 비밀번호 업데이트
