4df1d87caa3f02627654ca0fbf683c3711f06435
lidoca <666ltd@gmail.com>
2025-04-10 20:37:38 +0900

    fix(DTO): change Lombok annotation of TransactionArchiveDto to make templates can get data

--- src/main/java/com/onspring/onspring_customer/domain/common/dto/TransactionArchiveDto.java
+++ src/main/java/com/onspring/onspring_customer/domain/common/dto/TransactionArchiveDto.java
@@ -1,13 +1,17 @@
 package com.onspring.onspring_customer.domain.common.dto;
 
 import com.onspring.onspring_customer.domain.franchise.dto.FranchiseDto;
-import lombok.Value;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-@Value
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class TransactionArchiveDto implements Serializable {
     Long id;
     FranchiseDto franchiseDto;
