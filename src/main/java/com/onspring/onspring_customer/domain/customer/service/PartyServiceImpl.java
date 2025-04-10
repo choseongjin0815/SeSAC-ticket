449af3828afa4027e32a92750b09153ac122fef5
choseongin0815 <chobocho0815@gmail.com>
2025-04-10 20:54:54 +0900

    fix(service): add @Transactional

--- src/main/java/com/onspring/onspring_customer/domain/customer/service/PartyServiceImpl.java
+++ src/main/java/com/onspring/onspring_customer/domain/customer/service/PartyServiceImpl.java
@@ -22,6 +22,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.time.LocalTime;
