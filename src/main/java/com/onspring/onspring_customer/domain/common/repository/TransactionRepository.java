5e90f7bbcf9e69749f72eba22a6413b394e6f2cb
choseongin0815 <chobocho0815@gmail.com>
2025-04-10 12:09:18 +0900

    fix(repository): add constraint

--- src/main/java/com/onspring/onspring_customer/domain/common/repository/TransactionRepository.java
+++ src/main/java/com/onspring/onspring_customer/domain/common/repository/TransactionRepository.java
@@ -2,13 +2,11 @@ package com.onspring.onspring_customer.domain.common.repository;
 
 import com.onspring.onspring_customer.domain.common.entity.Transaction;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.domain.Pageable;
 import org.springframework.lang.NonNull;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -40,7 +38,8 @@ public interface TransactionRepository extends JpaRepository<Transaction, Long> 
 
     @Query("SELECT t FROM Transaction t WHERE t.franchise.id = :franchiseId " +
             "AND t.transactionTime BETWEEN :startDate AND :endDate " +
-            "ORDER BY t.id DESC")
+            "ANd t.isClosed = true " +
+            "ORDER BY t.id DESC" )
     List<Transaction> findTransactionsByFranchiseIdAndDateRangeAndClosed(
             @Param("franchiseId") Long franchiseId,
             @Param("startDate") LocalDateTime startDate,
