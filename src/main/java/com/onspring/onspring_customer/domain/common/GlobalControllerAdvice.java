e1ea5e3e42b01caeb09548818ee363050ac4b257
choseongin0815 <chobocho0815@gmail.com>
2025-04-10 21:09:52 +0900

    fix(advice): Update GlobalControllerAdvice to add userName to model only for authenticated users

--- src/main/java/com/onspring/onspring_customer/domain/common/GlobalControllerAdvice.java
+++ src/main/java/com/onspring/onspring_customer/domain/common/GlobalControllerAdvice.java
@@ -1,6 +1,5 @@
 package com.onspring.onspring_customer.domain.common;
 
-
 import com.onspring.onspring_customer.domain.customer.dto.AdminDto;
 import com.onspring.onspring_customer.domain.customer.service.AdminService;
 import com.onspring.onspring_customer.security.SecurityUtil;
@@ -8,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @RequiredArgsConstructor
 @ControllerAdvice
@@ -17,11 +18,20 @@ public class GlobalControllerAdvice {
 
     @ModelAttribute
     public void addUserNameToModel(Model model) {
-        Long adiminId = SecurityUtil.getCurrentUserId();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
-        AdminDto adminDto = adminService.findAdminById(adiminId);
-
-        model.addAttribute("userName", adminDto.getUserName());
+        // 로그인한 사용자만 처리
+        if (authentication != null && authentication.isAuthenticated()) {
+            try {
+                Long adminId = SecurityUtil.getCurrentUserId();
+                AdminDto adminDto = adminService.findAdminById(adminId);
+                model.addAttribute("userName", adminDto.getUserName());
+            } catch (Exception e) {
+                model.addAttribute("userName", "Guest");
+            }
+        } else {
+            // 비로그인 상태일 경우 (Guest로 처리 등)
+            model.addAttribute("userName", "Guest");
+        }
     }
-
 }
