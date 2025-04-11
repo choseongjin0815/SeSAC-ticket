eed9dbd97184dfb21137b801c700b80ead08f35f
lidoca <666ltd@gmail.com>
2025-04-11 15:22:03 +0900

    fix(DTO): add PartyDto to TransactionDto

--- src/main/java/com/onspring/onspring_customer/domain/common/dto/TransactionDto.java
+++ src/main/java/com/onspring/onspring_customer/domain/common/dto/TransactionDto.java
@@ -1,5 +1,6 @@
 package com.onspring.onspring_customer.domain.common.dto;
 
+import com.onspring.onspring_customer.domain.customer.dto.PartyDto;
 import com.onspring.onspring_customer.domain.franchise.dto.FranchiseDto;
 import com.onspring.onspring_customer.domain.user.dto.EndUserDto;
 import lombok.Data;
@@ -12,6 +13,7 @@ import java.time.LocalDateTime;
 public class TransactionDto implements Serializable {
     Long id;
     FranchiseDto franchiseDto;
+    PartyDto partyDto;
     EndUserDto endUserDto;
     LocalDateTime transactionTime;
     BigDecimal amount;
