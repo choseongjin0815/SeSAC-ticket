6b6f09c637f13ec916fcfa417f4460957ae64ee9
choseongin0815 <chobocho0815@gmail.com>
2025-04-14 11:07:38 +0900

    feat: 이미지 출력 기능

--- onspring-user-app/src/component/franchise/FranchiseMenuComponent.js
+++ onspring-user-app/src/component/franchise/FranchiseMenuComponent.js
@@ -1,44 +1,41 @@
-// import { API_SERVER_HOST, getMyInfo } from "../../api/myPageApi";
 import { useState, useEffect } from "react";
+import { useRoute } from "@react-navigation/native";
 import { View, Image, ScrollView, StyleSheet } from "react-native";
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import { API_SERVER_HOST, getFranchiseInfo } from "../../api/franchiseApi";
 
 const FranchiseMenuComponent = () => {
-    // const [franchise, setFranchise] = useState(null);
-    // const host = API_SERVER_HOST;
+    const route = useRoute();
+    const { franchiseId } = route.params || {};
+    const host = API_SERVER_HOST;
 
-    // useEffect(() => {
-    //     getMyInfo().then((data) => {
-    //         setFranchise(data);
-    //         console.log(data.uploadFileNames);
-    //     });
-    // }, []);
+    const [franchise, setFranchise] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+          if (!franchiseId) return;
+          try {
+            const info = await getFranchiseInfo(franchiseId);
+            setFranchise(info);
+          } catch (error) {
+            console.error("데이터 불러오기 실패", error);
+          }
+        };
+    
+        fetchData();
+    }, [franchiseId]);
 
     return (
         <ScrollView contentContainerStyle={styles.scrollContainer}>
-            {/* {franchise && franchise.uploadFileNames && franchise.uploadFileNames.map((imgFile, i) => (        
+            {franchise && franchise.uploadFileNames && franchise.uploadFileNames.map((imgFile, i) => (        
                 <View key={i} style={styles.imageWrapper}>
                     <Image 
-                        source={{ uri: `${host}/api/franchise/menu/${imgFile}` }} 
+                        source={{ uri: `${host}/api/user/menu/${imgFile}` }} 
                         style={styles.image}
                         resizeMode="contain"
                     />
                 </View>
-            ))} */}
-            {/* <View style={styles.imageWrapper}>
-                <Image
-                    source={require('../../../images/menu.jpg')}
-                    style={styles.image}
-                    resizeMode="contain"
-                />
-            </View> */}
-            <View style={styles.imageWrapper}>
-                <Image
-                    source={require('../../../images/menu.jpg')}
-                    style={styles.image}
-                    resizeMode="contain"
-                />
-            </View>
+            ))}
         </ScrollView>
     );
 }
