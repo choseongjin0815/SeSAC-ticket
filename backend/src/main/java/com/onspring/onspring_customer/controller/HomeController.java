3642885464fe677e84c34f6ff3d9378962ee7441
choseongin0815 <chobocho0815@gmail.com>
2025-06-30 20:24:10 +0900

    feat : 기본 경로 컨트롤러 추가

--- backend/src/main/java/com/onspring/onspring_customer/controller/HomeController.java
+++ backend/src/main/java/com/onspring/onspring_customer/controller/HomeController.java
@@ -2,9 +2,13 @@ package com.onspring.onspring_customer.controller;
 
 import com.onspring.onspring_customer.domain.customer.dto.AdminDto;
 import com.onspring.onspring_customer.domain.customer.service.AdminService;
+import com.onspring.onspring_customer.security.CustomUserDetails;
 import com.onspring.onspring_customer.security.SecurityUtil;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,11 +20,27 @@ public class HomeController {
 
     private final AdminService adminService;
 
+    @GetMapping("/")
+    public String root() {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        log.info("authentication: {}", authentication); // 객체 자체 확인
+        if (authentication != null &&
+                authentication.isAuthenticated() &&
+                authentication.getPrincipal() instanceof CustomUserDetails) {
+
+            return "redirect:/view/home"; // 로그인된 상태
+        } else {
+            return "auth/login"; // 로그인 안 된 상태
+        }
+    }
+
     @GetMapping("/view/home")
     public String home(Model model) {
-        Long adiminId = SecurityUtil.getCurrentUserId();
+        Long adminId = SecurityUtil.getCurrentUserId();
 
-        AdminDto adminDto = adminService.findAdminById(adiminId);
+        AdminDto adminDto = adminService.findAdminById(adminId);
         log.info(adminDto.getUserName());
 
         model.addAttribute("userName", adminDto.getUserName());
