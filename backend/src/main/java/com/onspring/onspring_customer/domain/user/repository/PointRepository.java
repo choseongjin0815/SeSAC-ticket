d37cf3c24b9847aab90ad186169f4821e678c969
choseongin0815 <chobocho0815@gmail.com>
2026-02-04 11:25:17 +0900

    refactor: 결제 처리 중 발생 가능한 Lost Update 방지를 위한 포인트 차감 로직 개선

--- backend/src/main/java/com/onspring/onspring_customer/domain/user/repository/PointRepository.java
+++ backend/src/main/java/com/onspring/onspring_customer/domain/user/repository/PointRepository.java
@@ -1,9 +1,13 @@
 package com.onspring.onspring_customer.domain.user.repository;
 
 import com.onspring.onspring_customer.domain.user.entity.Point;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.lang.NonNull;
 
 import java.util.Collection;
@@ -26,5 +30,10 @@ public interface PointRepository extends JpaRepository<Point, Long> {
     Optional<Point> findByParty_IdAndEndUser_Id(@NonNull Long partyId, @NonNull Long endUserId);
 
     long deleteByParty_IdAndEndUser_IdIn(@NonNull Long id, @NonNull Collection<Long> ids);
+
+    //포인트 결제 도중 관리자가 포인트 차감했을 때 처리
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select p from Point p where p.id = :id")
+    Optional<Point> findByIdForUpdate(@Param("id") Long id);
 
 }
